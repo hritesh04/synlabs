902bac50b3c5f2465871838b91e85825698860fc
hritesh04 <hritesh04.dev@gmail.com>
2024-10-17 16:31:42 +0530

    changed profile types to string

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -46,29 +46,29 @@ type Job struct {
 }
 
 type Profile struct {
-	ID                primitive.ObjectID `bson:"_id,omitempty" json:""id`
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	ApplicantID       primitive.ObjectID `bson:"applicant_id" json:"applicant_id"`
 	ResumeFileAddress string             `bson:"resume_file_address" json:"resume_file_address"`
-	Skills            []string           `bson:"skills" json:"skills"`
-	Education         []Education        `bson:"education" json:"education"`
-	Experience        []Experience       `bson:"experience" json:"experience"`
+	Skills            string             `bson:"skills" json:"skills"`
+	Education         string             `bson:"education" json:"education"`
+	Experience        string             `bson:"experience" json:"experience"`
 	Name              string             `bson:"name" json:"name"`
 	Email             string             `bson:"email" json:"email"`
 	Phone             string             `bson:"phone" json:"phone"`
 }
 
-type Education struct {
-	Institution  string `bson:"institution"`
-	Degree       string `bson:"degree"`
-	FieldOfStudy string `bson:"fieldOfStudy"`
-	StartDate    string `bson:"startDate"`
-	EndDate      string `bson:"endDate"`
-}
+// type Education struct {
+// 	Institution  string `bson:"institution"`
+// 	Degree       string `bson:"degree"`
+// 	FieldOfStudy string `bson:"fieldOfStudy"`
+// 	StartDate    string `bson:"startDate"`
+// 	EndDate      string `bson:"endDate"`
+// }
 
-type Experience struct {
-	Company     string `bson:"company"`
-	Title       string `bson:"title"`
-	Description string `bson:"description"`
-	StartDate   string `bson:"startDate"`
-	EndDate     string `bson:"endDate"`
-}
+// type Experience struct {
+// 	Company     string `bson:"company"`
+// 	Title       string `bson:"title"`
+// 	Description string `bson:"description"`
+// 	StartDate   string `bson:"startDate"`
+// 	EndDate     string `bson:"endDate"`
+// }
--- package/parser/parser.go
+++ package/parser/parser.go
@@ -26,12 +26,13 @@ func NewResumeParser(apikey, url string) *ResumeParser {
 }
 
 func (r *ResumeParser) Parse(file *multipart.FileHeader) (*domain.Profile, error) {
-	var profile domain.Profile
+	result := make(map[string]interface{})
 
 	content, err := file.Open()
 	if err != nil {
 		return nil, err
 	}
+	defer content.Close()
 	req, err := http.NewRequest(http.MethodPost, r.ParserUrl, content)
 	if err != nil {
 		return nil, err
@@ -40,13 +41,35 @@ func (r *ResumeParser) Parse(file *multipart.FileHeader) (*domain.Profile, error
 		req.Header.Add(key, header)
 	}
 	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("received non-OK response: %d", resp.StatusCode)
 	}
 	defer resp.Body.Close()
 	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&profile); err != nil {
+	if err := decoder.Decode(&result); err != nil {
 		return nil, err
 	}
-	return &profile, err
+	profile := &domain.Profile{
+		Name:              getString(result, "name"),
+		Email:             getString(result, "email"),
+		Phone:             getString(result, "phone"),
+		Education:         getString(result, "education"),
+		Skills:            getString(result, "skills"),
+		Experience:        getString(result, "experience"),
+		ResumeFileAddress: getString(result, "resume_file_address"),
+	}
+	return profile, nil
+}
+
+func getString(m map[string]interface{}, key string) string {
+	if value, ok := m[key]; ok {
+		if str, ok := value.(string); ok {
+			fmt.Println(str)
+			return str
+		}
+	}
+	return ""
 }
